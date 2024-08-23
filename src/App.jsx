e990ffa8614bfd0f5156312753c7b8b9f56ea727
Orion10101999 <orion10101999@gmail.com>
2024-08-23 21:33:43 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import ReviewList from "./components/ReviewList";
-import Test from "./components/Test";
-
 export default function App() {
   return (
     <>
-    <Test/>
       <h2 className="text-3xl my-5 text-green-600 bg-slate-300 p-5">Reviews : </h2>
       <ReviewList />
     </>
